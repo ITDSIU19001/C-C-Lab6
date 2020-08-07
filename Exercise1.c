c38486bc9dc523b3a52ec3670dffd4a1d26ce5c7
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2020-08-07 16:36:01 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -7,16 +7,49 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-int ex1(int testcase){
-	//your codes here
+void ex1(char *testcase) {
+	int len = strlen(testcase);
+	char *single_digit[] = { "zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine"};
+	char *tens_place[] = {"", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	char *tens_multiple[] = {"", "", "twenty", "thirty", "forty", "fifty","sixty", "seventy", "eighty", "ninety"};
+	char *tens_power[] = {"hundred", "thousand"};
+	if (len == 1) {
+		printf("%s\n", single_digit[*testcase - '0']);
+		return;
+	}
+	while (*testcase != '\0') {
+		if (len >= 3) {
+			if (*testcase -'0' != 0) {
+				printf("%s ", single_digit[*testcase - '0']);
+				printf("%s ", tens_power[len-3]); 
+			}
+         	--len;
+	}
+      else {
+		if (*testcase == '1') {
+			int sum = *testcase - '0' + *(testcase + 1)- '0';
+			printf("%s\n", tens_place[sum]);
+			return;
+         }
+         else if (*testcase == '2' && *(testcase + 1) == '0') {
+			printf("twenty\n");
+			return;
+         }
+         else {
+		int i = *testcase - '0';
+		printf("%s ", i? tens_multiple[i]: "");
+		++testcase;
+		if (*testcase != '0')
+		printf("%s ", single_digit[*testcase - '0']);
+         }
+      }
+      ++testcase;
+   }
 }
-
-int main(int argc, char *argv[]) {
-	int testcase = atoi(argv[1]);
-	
-	ex1(testcase);
-		
-	return 0;
+int main() {
+   char *testcase = 1234;
+   ex1(testcase);
+   return 0;
 }
